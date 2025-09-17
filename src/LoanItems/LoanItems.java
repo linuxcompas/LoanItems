cabdb45a0085d18bc5b7f4f358756931a7ccabde
linuxcompas <kalikeylogger00@gmail.com>
2025-09-17 09:52:43 +0200

    Super class

--- src/LoanItems/LoanItems.java
+++ src/LoanItems/LoanItems.java
@@ -1,4 +1,33 @@
 package LoanItems;
 
+//Attributes
 public class LoanItems {
-}
+    private String title;
+    private String type;
+
+//constructor
+    public String LoanItems(String title, String type) {
+        this.title = title;
+        this.type = type;
+
+    }
+//Here we have our getters
+        public String getType() {
+            return type;
+        }
+        public String getTitle() {
+            return title;
+        }
+//These are our setters
+
+        public void setTitle (String title) {this.title = title;}
+        public void setType (String type) {this.type = type;}
+
+    @Override
+    public String toString(String type, String title); {
+        return title + " " + type;
+
+
+
+            }
+        }
