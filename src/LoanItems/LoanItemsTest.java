ff42d3ba53058a91952b497efed28277fb82dec3
Esp <duckerlicious@duck.com>
2025-09-17 10:17:57 +0200

    en start på test

--- src/LoanItems/LoanItemsTest.java
+++ src/LoanItems/LoanItemsTest.java
@@ -1,4 +1,34 @@
 package LoanItems;
 
+import java.util.Scanner;
+
 public class LoanItemsTest {
+    public static void main(String[] args){
+
+    }
+
+    public void loanItems(){
+        // opretter scanner "sc"
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Welcome to the library");
+        System.out.println("How many loan items do you want to regiter?: ");
+        int numberOfLoanItems = sc.nextInt();
+
+
+        LoanItems[] Loanitems = new LoanItems[numberOfLoanItems];
+
+        for (int i = 0; i < numberOfLoanItems; i++){
+            System.out.println("Do you want to loan a book or video?: ");
+            String type = sc.nextLine();
+            System.out.println("What " + type + "do you want to loan?: ");
+            String name = sc.nextLine();
+            if (type.equalsIgnoreCase("book")) {
+
+            }
+            else if (type.equalsIgnoreCase("video")){
+
+            }
+        }
+    }
 }
