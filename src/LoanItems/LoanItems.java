12b189b24b2b7dece02d5249d248f098c4d48ded
linuxcompas <kalikeylogger00@gmail.com>
2025-09-17 11:49:52 +0200

    @overrride method

--- src/LoanItems/LoanItems.java
+++ src/LoanItems/LoanItems.java
@@ -5,36 +5,39 @@ public class LoanItems {
     private String title;
     private String type;
 
-//constructor
+    //constructor
     LoanItems(String title, String type) {
         this.title = title;
         this.type = type;
 
     }
-//Here we have our getters
-        public String getType() {
-            return type;
-        }
-        public String getTitle() {
-            return title;
-        }
+
+    //Here we have our getters
+    public String getType() {
+        return type;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 //These are our setters
 
-        public void setTitle (String title) {this.title = title;}
-        public void setType (String type) {this.type = type;}
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-    public void title(){
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void title() {
 
     }
 
     @Override
-    public String toString(String type, String title);{
-        return title + " " + type;
+    public String toString() {
+        return getTitle() + " " + "(" + getType() + ")";
 
-
-        //Loan items
-        System.out.println("LIST OF LOAN ITEMS");
-        for (LoanItems item : type + title)
-            System.out.println(type + "    " + title);
     }
 }
+     //Move method
