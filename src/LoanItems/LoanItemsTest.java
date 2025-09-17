f265a1ff9c8139e0e6a531856eb373ec2bbbfad5
Esp <duckerlicious@duck.com>
2025-09-17 11:47:51 +0200

    scanner fix

--- src/LoanItems/LoanItemsTest.java
+++ src/LoanItems/LoanItemsTest.java
@@ -7,32 +7,32 @@ public class LoanItemsTest {
         loanItems();
     }
 
-    public static void loanItems(){
+    public static void loanItems() {
         // opretter scanner "sc"
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Welcome to the library");
         System.out.println("How many loan items do you want to regiter?: ");
         int numberOfLoanItems = sc.nextInt();
-
+        sc.nextLine();
 
         LoanItems[] Loanitems = new LoanItems[numberOfLoanItems];
 
-        for (int i = 0; i < numberOfLoanItems; i++){
-            System.out.println("Do you want to loan a book or video?: " + (i + 1) + ": ");
+        for (int i = 0; i < numberOfLoanItems; i++) {
+            System.out.println("Do you want to loan a book or video?: ");
+            int c = 0;
             String type = sc.nextLine();
 
             if (type.equalsIgnoreCase("book")) {
-                System.out.println("What book do you want to loan?: " + (i + 1) + ": ");
+                System.out.println("What book do you want to loan?: " + (c + 1) + ": ");
                 String name = sc.nextLine();
 
-                Loanitems[i] = new Book(name, "");
-            }
-            else if (type.equalsIgnoreCase("video")){
-                System.out.println("What video do you want to loan?: ");
+                Loanitems[i] = new Book(name, "Book");
+            } else if (type.equalsIgnoreCase("video")) {
+                System.out.println("What video do you want to loan?: " + (c + 1) + ": ");
                 String name = sc.nextLine();
 
-                Loanitems[i] = new Video(name, "");
+                Loanitems[i] = new Video(name, "Video");
             }
         }
 
